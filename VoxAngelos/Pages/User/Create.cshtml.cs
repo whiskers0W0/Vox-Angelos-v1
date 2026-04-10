52ab52388351f9199ff0f7e0c85f777a0c6c160c
rainmklla <mikaellagomez102004@gmail.com>
2026-04-10 15:21:14 +0800

    citizen interface - no backend

---
+++ VoxAngelos/Pages/User/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VoxAngelos.Pages.User
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
